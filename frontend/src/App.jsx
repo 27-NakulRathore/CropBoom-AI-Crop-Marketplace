d5f1eb1d69801bc25b8ec5100229e471e8a80f66
sharmmohit <sharmmohit29@gmail.com>
2025-04-18 18:09:57 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,15 +7,19 @@ import BuyerRegistrationForm from './Components/BuyerRegistrationForm';
 import BuyerHomePage from './Components/BuyerHomePage';
 import FarmerHomePage from './Components/FarmerHomePage';
 import UploadCropForm from './Components/UploadCropForm';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing/>} />
+        <Route path="/" element={<Landing />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/register/farmer" element={<FarmerRegistrationForm />} />
         <Route path="/register/buyer" element={<BuyerRegistrationForm />} />
-        {/* ... other routes ... */}
+        <Route path="/BuyerHomePage" element={<BuyerHomePage />} /> {/* Route for BuyerHomePage */}
+        <Route path="/FarmerHomePage" element={<FarmerHomePage />} /> {/* Route for FarmerHomePage */}
+        <Route path="/farmer/upload-crop" element={<UploadCropForm />} /> {/* Example route */}
+        {/* Add other routes here as needed */}
       </Routes>
     </BrowserRouter>
   );
